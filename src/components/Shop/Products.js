fe19835059cdc9fc6e58cf815eabbd73a5265dbb
Babashov Mahammad <babashov.info@gmail.com>
2023-02-06 19:59:43 +0400

    cartSlice is ready

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,38 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_DATA = [
+  {
+    id:1,
+    title:'Product 1',
+    price:5,
+    description:'Description of Product 1'
+  },
+  {
+    id:2,
+    title:'Product 2',
+    price:10,
+    description:'Product 2 Description'
+  }  
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map(item=>(
+
+          <ProductItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          price={item.price}
+          description={item.description}
+          />
+
+        ))}
+        
       </ul>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ui from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-    reducer:{ui:ui.reducer}
+    reducer:{ui:ui.reducer,cart:cartSlice.reducer}
 })
 
 export default store
